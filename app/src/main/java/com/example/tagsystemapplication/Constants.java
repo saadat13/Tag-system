9a70dea2e44a2065463b96f4abf450c14718bd5a
saadat13 <saadathassan34@gmail.com>
2019-07-19 03:51:54 +0430

    add tag seems to work correctly and scroll improved

---
+++ app/src/main/java/com/example/tagsystemapplication/Constants.java
@@ -0,0 +1,15 @@
+package com.example.tagsystemapplication;
+
+public class Constants {
+
+    public static final int UNDEFINED = -1;
+    public static final int EXPERT = 0;
+    public static final int FULL_EXPERT = 1;
+    public static final int MANAGER = 2;
+
+
+    public static final int IMAGE = 10;
+    public static final int VIDEO = 100;
+    public static final int STRING = 1000;
+
+}
